8bab3158749faa8be6ffe7a1991d24b36be4c206
Gerson Lucas <114064009+gelks8@users.noreply.github.com>
2023-11-10 12:59:21 -0300

    img for products

--- supermarket-mobile/components/Home.tsx
+++ supermarket-mobile/components/Home.tsx
@@ -6,11 +6,11 @@ import { useNavigation } from '@react-navigation/native';
 import { Card } from 'react-native-elements';
 
 const productList = [
-    {id: 0, nome: "Arroz", preco: 4.99, qnt: 1000, img: ""},
-    {id: 0, nome: "feijão", preco: 6.59, qnt: 1420, img: ""},
-    {id: 0, nome: "Cuscuz", preco: 2.89, qnt: 578, img: ""},
-    {id: 0, nome: "Macarrão", preco: 2.59, qnt: 687, img: ""},
-    {id: 0, nome: "Cafe", preco: 4.59, qnt: 800, img: ""},
+    {id: 0, nome: "Arroz", preco: 4.99, qnt: 1000, img: "https://propao.agilecdn.com.br/2314_1.jpg"},
+    {id: 0, nome: "feijão", preco: 6.59, qnt: 1420, img: "https://io.convertiez.com.br/m/trimais/shop/products/images/891/medium/feijao-carioca-tipo-1-broto-legal-pacote-1kg_886.jpg"},
+    {id: 0, nome: "Cuscuz", preco: 2.89, qnt: 578, img: "https://ibassets.com.br/ib.item.image.big/b-605a47a63e3c4708a95c063eea0ea3ee.jpeg"},
+    {id: 0, nome: "Macarrão", preco: 2.59, qnt: 687, img: "https://redemachado.com.br/arquivos/525930.jpg"},
+    {id: 0, nome: "Cafe", preco: 4.59, qnt: 800, img: "https://propao.agilecdn.com.br/5188_1.jpg"},
 ]
 
 const Home = () => {
@@ -30,6 +30,11 @@ const Home = () => {
             <Text style={styles.textTitle}>Bem-vindo à loja</Text>
             {productList.map((product, index) => (
                 <Card key={index} containerStyle={{ marginBottom: 16, alignItems: 'center' }}>
+                <Image
+                source={{ uri: product.img }}
+                style={{ height: 200 }}
+                resizeMode="cover"
+                />
                 <Text style={{ marginTop: 10, marginBottom: 10, fontWeight: 'bold' }}>
                     Nome: {product.nome}
                 </Text>
